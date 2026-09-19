86304c94e54b1c6bd880d074193a1d085337c3b2
Joao-vithor-1 <joaopvp7778@gmail.com>
2026-09-19 16:35:31 -0300

    -Resolvi a issue T018 -criei a classe PubicacaoInstitucionalGet, feita para pegar os dados do banco de dados -adicionei o route com endereço temporario,vou trocar depois, e criei uma funçao de entrada e outra de saida

--- backend/app/api/schema/publicacao_institucional.py
+++ backend/app/api/schema/publicacao_institucional.py
@@ -1,9 +1,13 @@
 from datetime import datetime
-
+from fastapi import APIRouter, HTTPException
 from pydantic import field_validator, HttpUrl
-from sqlmodel import SQLModel, Field, AutoString
+from sqlmodel import SQLModel, Field, AutoString,Session, create_engine,select
 
+publicacao_router = APIRouter()
 
+sqlite_file_name = "database_publicaco_institucional.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url)
 class PublicacaoInstitucionalBase(SQLModel):
     fonte_id: int
     titulo: str
@@ -48,3 +52,34 @@ class PublicacaoInstitucional(
 
 class PublicacaoInstitucionalCreate(PublicacaoInstitucionalBase):
     pass
+
+class PublicacaoInstitucionalGet(SQLModel):
+    publicacao_id : int = Field(default = None,foreign_key="PublicacaoInstitucional.id")
+    fonte_id: int
+    titulo: str
+    resumo: str
+    categoria: str
+    unidade_responsavel: str
+    url_oficial: str
+
+@publicacao_router.get("/{publicacao_id}")
+async def get_publicacao(publicacao_id: int):
+    with Session(engine) as session:
+        publicacao = session.get(PublicacaoInstitucional,publicacao_id)
+        if not publicacao:
+            raise HTTPException(status_code=404,
+                                detail=f"publicação com Id {publicacao_id} não existe")
+        return publicacao
+
+
+@publicacao_router.post("/criar")
+async def post_publicacao(publicacao: PublicacaoInstitucionalCreate):
+    with Session(engine) as session:
+        if not publicacao:
+            raise HTTPException(status_code=422,
+                                detail="Erro de entrada, faltou parametro ou tipo errado")# colocar mais detalhes depois
+        publicacao_db = PublicacaoInstitucional.model_validate(publicacao)
+        session.add(publicacao_db)
+        session.commit()
+        session.refresh(publicacao_db)
+        return publicacao_db
