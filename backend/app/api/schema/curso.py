86e922aa9dade8dec95d922d79cbed2dbe222a7b
Joao-vithor-1 <joaopvp7778@gmail.com>
2026-09-25 19:01:51 -0300

    Implementei as classes get em disciplina.py - Criei as classes GetListaCurso,post_disciplina,get_disciplina - adicionei mais algums schemas em cliente e disciplina. -Resolvi a Issue T026

--- backend/app/api/schema/curso.py
+++ backend/app/api/schema/curso.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field
-
+from sqlalchemy import Column, JSON
+from typing import List
 
 class CursoBase(SQLModel):
     nome: str
@@ -9,11 +10,16 @@ class CursoBase(SQLModel):
 
 class Curso(CursoBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    disciplina_id: List[int] = Field(default_factory=list, sa_column=Column(JSON))
+
 
 
 class CursoCreate(CursoBase):
+
     pass
 
-class CursoGet(SQLModel):
-    sigla : str | None
-    nome : str | None
+class CursoGet(CursoBase):
+    disciplina_id: List[int] = Field(default_factory=list, sa_column=Column(JSON))
+
+
+
